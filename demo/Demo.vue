a1cc45c216f266f0b4827aaae8ba3fee6c339269
姜灏 <150haomengting@163.com>
2018-08-25 09:13:17 +0800

    init project

---
+++ demo/Demo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>v-panel</p>
+        <v-panel :show="show" :position="'center'">
+            <div slot="panel">
+                <div class="x100">
+                    <p class="container ym20 x-between">
+                        <span class="b-title">产品信息</span>
+                        <span @click="show = 0">X</span>
+                    </p>
+                </div>
+            </div>
+        </v-panel>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            show: 1
+        }
+    }
+}
+</script>
+<style scoped></style>
---
+++ src/Panel.vue
@@ -0,0 +1,22 @@
+<template>
+    <article class="bgf p-fixed o-auto" :class="{
+        'x100 h100 xl0 yt0':position==='center', // center
+        'x100 y320 xl0 yt0 b-bottom':position==='top', // top
+        'x25 h100 xr0 yt0 b-left':position==='right', // right
+        'x100 y320 xl0 yb0 b-top':position==='bottom', // bottom
+        'x25 h100 xl0 yt0 b-right':position==='left', // left
+        }" v-show="show">
+        <div class="x92">
+            <slot name="panel"></slot>
+        </div>
+    </article>
+</template>
+<script>
+export default {
+    props: ["show", "position"],
+    data() {
+        return {}
+    }
+}
+</script>
+<style scoped></style>
